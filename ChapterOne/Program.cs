77d6993db62f778431f2441d5f910f0d0b94c13b
Bazza <bazzaware@gmail.com>
2019-11-16 22:32:04 +0000

    Full code

--- ChapterOne/Program.cs
+++ ChapterOne/Program.cs
@@ -19,6 +19,53 @@ namespace ChapterOne
                 Console.ReadKey(false);
                 return;
             }
+            while (choice != 4)
+            {
+                Console.WriteLine("************************** Welcome to Banking App ****************************\n");
+                Console.WriteLine("1. Check Balance:\n");
+                Console.WriteLine("2. Withdraw Cash:\n");
+                Console.WriteLine("3. Deposit Cash:\n");
+                Console.WriteLine("4. Quit\n");
+                Console.WriteLine("************************** Choose and option ****************************\n");
+
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice) {
+                    case 1:
+                        Console.WriteLine("\nYour balance is £ {0}", balance);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nEnter the amount you want to withdraw?");
+                        withdrawAmt = int.Parse(Console.ReadLine());
+                        if (withdrawAmt % 100 != 0)
+                        {
+                            Console.WriteLine("\nDenominations available 100, 500, 2000.  Your amount count not be processed.");
+                        }
+                        else if (withdrawAmt > balance)
+                        {
+                            Console.WriteLine("/n Insufficiant funds");
+                        }
+                        else
+                        {
+                            balance = balance - withdrawAmt;
+                            Console.WriteLine("\n\nYour transaction has been processed");
+                            Console.WriteLine("\nYour balance is £{0}", balance);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\nEnter the amount you wish to deposit?" );
+                        depositeAmt = int.Parse(Console.ReadLine());
+                        balance = balance + depositeAmt;
+                        Console.WriteLine("\nYour deposit has been processed.");
+                        Console.WriteLine("\nYour balance is £{0}",balance);
+                        break;
+                    case 4:
+                        Console.WriteLine("\nThank you for using Bazza bank.");
+                        break;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
